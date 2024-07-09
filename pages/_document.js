19d2bf4590c1733997e70b8da81d7b1e2a4fc027
Molar <grubenmj@gmail.com>
2024-07-08 22:43:21 -0600

    first commit

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <body className="bg-[#F5F5F5]">
         <Main />
         <NextScript />
       </body>
