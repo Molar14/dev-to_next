088911b8ec887d5f5ed0a5d1a9f7c4ba519b6517
Molar <grubenmj@gmail.com>
2024-07-13 00:31:24 -0600

    done

--- pages/index.js
+++ pages/index.js
@@ -8,14 +8,14 @@ import { toast } from "sonner";
 //components
 export default function Home () {
     return (
-        <main className="w-full min-h-screen bg-white grid grid-cols-[20%_3fr_20%] grid-rows-[4rem_3fr] text-black  ">
+        <main className="w-full min-h-screen bg-white grid grid-cols-[20%_3fr_20%] grid-rows-[4rem_3fr] text-black gap-5  ">
             
-            <header className="col-span-3"> 
+            <header className="col-span-3 pt-1"> 
                 <Navbar />
             </header>
             
             
-            <aside className="row-span-2">
+            <aside className="row-span-2 pl-20">
                 <Aside/>
             </aside>
 
@@ -23,7 +23,7 @@ export default function Home () {
                 <Content />
             </section>
 
-            <aside className="row-span-2">
+            <aside className="row-span-2 pr-20">
                 <AsideRight/>
             </aside>
           
